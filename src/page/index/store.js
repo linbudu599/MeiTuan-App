c885e3747df3b5ecd8ef460dcbd6fe0245393655
linbudu599 <linbudu599@gmail.com>
2020-01-20 11:15:48 +0800

    :arrow_up: deps: update and add essential deps

--- src/page/index/store.js
+++ src/page/index/store.js
@@ -1,32 +1,28 @@
-import { createStore,applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from "redux";
 
-import mainReducer from './reducers/main.js';
+import mainReducer from "./reducers/main.js";
 
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
-import createHistory from 'history/createHashHistory'
+import createHistory from "history/createHashHistory";
 
-import { routerMiddleware } from 'react-router-redux'
+import { routerMiddleware } from "react-router-redux";
 
 // 创建基于hash的history
 const history = createHistory();
 
 // 创建初始化tab
-history.replace('home');
+history.replace("home");
 
 // 创建history的Middleware
 const historyMiddl = routerMiddleware(history);
 
-const store = createStore(mainReducer, applyMiddleware(thunk,historyMiddl));
-
+const store = createStore(mainReducer, applyMiddleware(thunk, historyMiddl));
 
 if (module.hot) {
-    module.hot.accept('./reducers/main', ()=>{
-        const nextRootReducer = require('./reducers/main.js').default;
-        store.replaceReducer(nextRootReducer)
-    });
+  module.hot.accept("./reducers/main", () => {
+    const nextRootReducer = require("./reducers/main.js").default;
+    store.replaceReducer(nextRootReducer);
+  });
 }
-module.exports = {
-    store,
-    history
-}
+export { store, history };
